ecf9e7895e2f1c8e114a95b727dd8494a8719933
bret <bretai@LCARS-MX-23>
2024-03-14 13:26:18 +0300

    Room toolbar

---
+++ app/board/[boardId]/_components/tool-button.tsx
@@ -0,0 +1,33 @@
+import { LucideIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import Hint from "@/components/hint";
+
+export interface IToolButtonProps {
+  label: string;
+  icon: LucideIcon;
+  onClick: () => void;
+  isActive?: boolean;
+  isDisabled?: boolean;
+}
+
+export default function ToolButton({
+  label,
+  icon: Icon,
+  onClick,
+  isActive,
+  isDisabled,
+}: IToolButtonProps) {
+  return (
+    <Hint label={label} side="right" sideOffset={14}>
+      <Button
+        disabled={isDisabled}
+        onClick={onClick}
+        size="icon"
+        variant={isActive ? "boardActive" : "board"}
+      >
+        <Icon />
+      </Button>
+    </Hint>
+  );
+}

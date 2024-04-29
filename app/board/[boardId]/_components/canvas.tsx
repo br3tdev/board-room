2119d48b18aaf572477209a21a428730e91e0bf3
bret <bretai@LCARS-MX-23>
2024-04-29 17:05:52 +0300

    Color change && Deletion

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -34,6 +34,7 @@ import Info, { InfoSkeleton } from "./info";
 import { LayerPreview } from "./layer-preview";
 import Participants from "./participants";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 import Toolbar from "./toolbar";
 
 const MAX_LAYERS = 100;
@@ -284,6 +285,7 @@ export default function Canvas({ boardId }: ICanvasProps) {
         undo={history.undo}
         redo={history.redo}
       />
+      <SelectionTools camera={camera} setLastUsedColor={setLastUsedColor} />
       <svg
         className="h-[100%] w-[100%]"
         onWheel={onWheel}

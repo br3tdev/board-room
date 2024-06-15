3f9af28eb1e04cb0bbb45077e7a1889ad0efd415
bret <bretai@LCARS-MX-23>
2024-06-15 13:25:54 +0300

    Update favicon and site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-import ConvexClientProvider from '@/providers/convex-client-provider';
+import ConvexClientProvider from "@/providers/convex-client-provider";
 
-import './globals.css';
+import "./globals.css";
 
-import ModalProvider from '@/providers/modal-providers';
+import ModalProvider from "@/providers/modal-providers";
 
-import { Toaster } from '@/components/ui/sonner';
+import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Board-room',
-  description: 'Productivity and collaborations to new heights',
+  title: "Board-room",
+  description: "Productivity and collaborations to new heights",
 };
 
 export default function RootLayout({
